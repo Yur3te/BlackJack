42a06916ab4725148cabd6f05c2ac6eb240e6ca1
Yur3te <yur3te@gmail.com>
2025-03-17 20:54:11 +0100

    added example usage for deck of cards and testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include "include/Game.h"
 
+#include "include/Deck.h"
+
+using namespace std;
+
 int main() {
-    std::cout << "Let the coding begin!" << std::endl;
+
+    Deck deck;
+    cout << "-----SHUFFLED DECK-----" << endl;
+    deck.shuffle();
+    deck.printDeck();
+    cout << "-----DEALING EXAMPLE HOLDEM HAND-----" << endl;
+    for (int i = 0; i < 2; i++) {
+        deck.deal()->printCard();
+    }
     return 0;
 }
