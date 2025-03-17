16016e472760e796539d0b2174250d7fc4ac0a4a
Yur3te <yur3te@gmail.com>
2025-03-17 19:38:23 +0100

    Created Deck class

--- include/Deck.h
+++ include/Deck.h
@@ -1,4 +1,21 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include <iostream>
+#include "Card.h"
+#include <vector>
+
+using namespace std;
+
+class Deck {
+    public:
+        Deck();
+        ~Deck();
+        void printDeck();
+        void shuffle();
+        void deal();
+    private:
+        vector<Card*> deck;
+};
+
 #endif // DECK_H
