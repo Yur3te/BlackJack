f6ab3dcea914b3b18c6d762a6a7a0853480180fd
Yur3te <yur3te@gmail.com>
2025-03-17 19:27:30 +0100

    Created Card class and functionality

--- include/Card.h
+++ include/Card.h
@@ -1,4 +1,25 @@
 #ifndef CARD_H
 #define CARD_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum class Suit { Hearts, Diamonds, Clubs, Spades };
+enum class Rank {Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King,};
+
+class Card {
+    public:
+        Card();
+        ~Card();
+        Card(Rank rank, Suit suit);
+        int getValue() const;
+        void printCard();
+        string toString() const;
+    private:
+        Rank rank;
+        Suit suit;
+};
+
 #endif // CARD_H
--- src/Card.cpp
+++ src/Card.cpp
@@ -1,1 +1,21 @@
 #include "../include/Card.h"
+
+#include <iostream>
+
+using namespace std;
+
+Card::Card(Rank rank, Suit suit) : rank(rank), suit(suit) {}
+
+Card::~Card() {}
+
+int Card::getValue() const {
+    if (rank >= Rank::Two && rank <= Rank::Ten) return static_cast<int>(rank);
+    if (rank >= Rank::Jack && rank <= Rank::King) return 10;
+    return 11;
+}
+
+string Card::toString() const {
+    const string suits[] = {"♥", "♦", "♣", "♠"};
+    const string ranks[] = {"", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    return ranks[static_cast<int>(rank)] + suits[static_cast<int>(suit)];
+}
