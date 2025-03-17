1f7f7dd11e86a0309ce4eef2fd33817cbffebc55
Yur3te <yur3te@gmail.com>
2025-03-17 20:39:16 +0100

    Fixed by adding const to printCard() function

--- include/Card.h
+++ include/Card.h
@@ -7,7 +7,7 @@
 using namespace std;
 
 enum class Suit { Hearts, Diamonds, Clubs, Spades };
-enum class Rank {Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King,};
+enum class Rank {Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King};
 
 class Card {
     public:
@@ -15,7 +15,7 @@ class Card {
         ~Card();
         Card(Rank rank, Suit suit);
         int getValue() const;
-        void printCard();
+        void printCard() const;
         string toString() const;
     private:
         Rank rank;
