657304fe92e77afce8e50e893baa691556794028
Yur3te <yur3te@gmail.com>
2025-03-18 20:39:44 +0100

    cleanup of unused includes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include "include/Game.h"
 
-#include "include/Deck.h"
-#include "include/Player.h"
-#include "include/Dealer.h"
-
 using namespace std;
 
 int main() {
-
     Game game;
     game.play();
     return 0;
