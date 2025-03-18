bfba581861a578aa619cce6ed305fc55ae2b633c
Yur3te <yur3te@gmail.com>
2025-03-18 17:06:29 +0100

    basic blackjack testing

--- main.cpp
+++ main.cpp
@@ -2,18 +2,39 @@
 #include "include/Game.h"
 
 #include "include/Deck.h"
+#include "include/Player.h"
+#include "include/Dealer.h"
 
 using namespace std;
 
 int main() {
 
     Deck deck;
-    cout << "-----SHUFFLED DECK-----" << endl;
     deck.shuffle();
-    deck.printDeck();
-    cout << "-----DEALING EXAMPLE HOLDEM HAND-----" << endl;
-    for (int i = 0; i < 2; i++) {
-        deck.deal()->printCard();
-    }
+    // deck.printDeck();
+    Dealer dealer;
+    Player player;
+
+    cout << "Dealing cards..." << endl;
+
+    player.addCard(deck.deal());
+
+    dealer.addCard(deck.deal());
+
+    player.addCard(deck.deal());
+
+    dealer.addCard(deck.deal());
+
+    dealer.printFirstCard();
+
+    cout << "Your hand: " << endl;
+    player.printHand();
+    cout<<"Hand value: " <<player.getHandValue()<<endl;
+    player.playTurn(deck);
+
+    
+    dealer.playTurn(deck);
+
+
     return 0;
 }
