5aa985c4d7dde1972267a4728f293f177bc7f6a8
Yur3te <yur3te@gmail.com>
2025-03-18 16:44:49 +0100

    added Person for Player and Dealer inheritance

--- include/Person.h
+++ include/Person.h
@@ -1,4 +1,21 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <vector>
+#include "Card.h"
+
+class Person {
+    public:
+        Person();
+        virtual ~Person();
+        void addCard(Card* card);
+        void printHand() const;
+        int getHandValue() const;
+        bool isBusted() const;
+    protected:
+        vector<Card*> hand;
+};
+
+
+
 #endif // PERSON_H
--- src/Person.cpp
+++ src/Person.cpp
@@ -1,1 +1,42 @@
 #include "../include/Person.h"
+
+#include <iostream>
+
+
+Person::Person() {}
+
+void Person::addCard(Card* card) {
+    hand.push_back(card);
+}
+
+void Person::printHand() const {
+    for (Card* card : hand) {
+        card->printCard();
+    }
+}
+
+int Person::getHandValue() const {
+    int sum = 0;
+    int numAces = 0;
+    for (Card* card : hand) {
+        sum += card->getValue();
+        if (card->getValue() == 11) {
+            numAces++;
+        }
+    }
+    while (sum > 21 && numAces > 0) {
+        sum -= 10;
+        numAces--;
+    }
+    return sum;
+}
+
+bool Person::isBusted() const {
+    return getHandValue() > 21;
+}
+
+Person::~Person() {
+    for (Card* card : hand) {
+        delete card;
+    }
+}
