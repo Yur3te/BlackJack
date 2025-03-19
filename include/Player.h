fe063f7f5b42a658c3e8d9649cde5c2b32a89470
Yur3te <yur3te@gmail.com>
2025-03-19 21:27:20 +0100

    added double down

--- include/Player.h
+++ include/Player.h
@@ -3,10 +3,13 @@
 
 #include "Person.h"
 #include "Deck.h"
+#include "Chips.h"
 
 class Player : public Person {
     public:
-        void playTurn(Deck& deck);
+        void playTurn(Deck& deck, int& bet, Chips& chips);
+        bool doubleDown(Deck& deck, int& bet, Chips& chips);
+        // bool Split() const;
 };
 
 #endif // PLAYER_H
--- src/Player.cpp
+++ src/Player.cpp
@@ -2,24 +2,51 @@
 
 #include <iostream>
 
-void Player::playTurn(Deck& deck) {
-    char choice;
+#include "../include/Chips.h"
+
+void Player::playTurn(Deck& deck, int& bet, Chips& chips) {
     while (true) {
-        cout << "Do you want to hit or stand? (h/s): ";
+        char choice;
+        cout << "Do you want to hit, stand or double? (h/s/d): ";
         cin >> choice;
         if (choice == 'h') {
-            addCard(deck.deal());
-            cout << "Your hand: " << endl;
-            printHand();
-            cout << "Hand value: " << getHandValue() << endl;
+            Card* newCard = deck.deal();
+            addCard(newCard);
+            cout << "You drew: ";
+            newCard->printCard();
+            cout << "New hand value: " << getHandValue() << endl;
             if (isBusted()) {
                 cout << "You busted!" << endl;
                 break;
             }
-        } else if (choice == 's') {
+        } else if (choice == 's'){
+            cout << "You chose to stand!" << endl;
+            break;
+        } else if (choice == 'd') {
+            doubleDown(deck, bet, chips);
             break;
         } else {
             cout << "Invalid choice!" << endl;
         }
     }
 }
+
+bool Player::doubleDown(Deck& deck, int& bet, Chips& chips) {
+    if (bet * 2 > chips.getChips()) {
+        cout << "Not enough chips to double down!" << endl;
+        return false;
+    }
+    
+    chips.removeChips(bet); // Deduct the extra bet amount
+    bet *= 2; // Double the bet
+
+    cout << "You chose to double down! You receive one final card." << endl;
+    Card* newCard = deck.deal();
+    addCard(newCard);
+    cout << "You drew: ";
+    newCard->printCard();
+    cout << "New hand value: " << getHandValue() << endl;
+    
+    return true;
+}
+
