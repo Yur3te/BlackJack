537403505a6d601ed5de70b32588a1cc6102067e
Yur3te <yur3te@gmail.com>
2025-05-27 15:25:51 +0200

    Simplified dealer playTurn() function for UI purposes

--- src/Dealer.cpp
+++ src/Dealer.cpp
@@ -4,19 +4,11 @@
 
 
 void Dealer::playTurn(Deck& deck) {
-    cout << "Dealer's hand: " << endl;
-    printHand();
-    cout << "Hand value: " << getHandValue() << endl;
     while (getHandValue() < 17) {
         Card* newCard = deck.deal();
         addCard(newCard);
-        cout << "Dealer drew: ";
-        newCard->printCard();
-        cout << "Hand value: " << getHandValue() << endl;
     }
-    if (isBusted()) {
-        cout << "Dealer busted!" << endl;
-    }
+
 }
 
 void Dealer::printFirstCard() const {
